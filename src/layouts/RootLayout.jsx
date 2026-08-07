d9195eb6e4d9fcc83c6933dea4fe3aec1e3b07c8
Samantha Richards <samantha.y.richards@gmail.com>
2026-08-07 14:45:28 -0400

    removed Rich link preview - broke code

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,10 +1,7 @@
 import { Outlet } from 'react-router'
-import { useShareMeta } from '../hooks/useShareMeta'
 import './RootLayout.css'
 
 export default function RootLayout() {
-  useShareMeta()
-
   return (
     <div className="app-root">
       <Outlet />
